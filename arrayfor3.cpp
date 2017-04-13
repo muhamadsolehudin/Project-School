b8569c65dc4e0527fd7703903140d2329cb17f1f
muhamadsolehudin <muhammad.solehhudin806@gmail.com>
2017-04-13 16:57:39 +0700

    array and for

---
+++ arrayfor3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	//Kamus
+	string nama[3];
+	int i;
+	
+	//Algoritma
+	for (i=0; i<3; i++){
+	cin>>nama[i];
+	}
+	for (i=0; i<3; i++){
+		
+		cout<<nama[i]<<endl;
+	}
+	
+}
