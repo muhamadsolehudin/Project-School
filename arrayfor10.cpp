1de0b06709df8bbb978bf5026967d6dcc88c9375
muhamadsolehudin <muhammad.solehhudin806@gmail.com>
2017-04-13 16:58:25 +0700

    array and for II

---
+++ arrayfor10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	//Kamus
+	string nama[10];
+	int i;
+	
+	//Algoritma
+	for (i=0; i<10; i++){
+	cin>>nama[i];
+	}
+	cout<<"---------------------------"<<endl;
+	for (i=0; i<10; i++){
+		
+		cout<<nama[i]<<endl;
+	}
+	
+}
