08ddbe4aa53da1574be58b05bcda5bd1b0e7d903
muhamadsolehudin <muhammad.solehhudin806@gmail.com>
2017-04-13 16:55:50 +0700

    belajar array

---
+++ array3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	//Kamus
+	string nama[3];
+	
+	//Algoritma
+		cin>>nama[0];
+		cin>>nama[1];
+		cin>>nama[2];
+	cout<<nama[0]<<endl;
+	cout<<nama[1]<<endl;
+	cout<<nama[2]<<endl;
+}
