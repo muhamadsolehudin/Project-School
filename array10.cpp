2b53ae1a690a61663298dfde6135592abb99b1a7
muhamadsolehudin <muhammad.solehhudin806@gmail.com>
2017-04-13 16:56:27 +0700

    belajar array II

---
+++ array10.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	//Kamus
+	string nama[10];
+	//Algoritma
+	cin>>nama[0];
+	cin>>nama[1];
+	cin>>nama[2];
+	cin>>nama[3];
+	cin>>nama[4];
+	cin>>nama[5];
+	cin>>nama[6];
+	cin>>nama[7];
+	cin>>nama[8];
+	cin>>nama[9];
+	
+	cout<<nama[0]<<"-"<<nama[0]<<endl;
+	cout<<nama[0]<<"-"<<nama[1]<<endl;
+	cout<<nama[0]<<"-"<<nama[2]<<endl;
+	cout<<nama[0]<<"-"<<nama[3]<<endl;
+	cout<<nama[0]<<"-"<<nama[4]<<endl;
+	cout<<nama[0]<<"-"<<nama[5]<<endl;
+	cout<<nama[0]<<"-"<<nama[6]<<endl;
+	cout<<nama[0]<<"-"<<nama[7]<<endl;
+	cout<<nama[0]<<"-"<<nama[8]<<endl;
+	cout<<nama[0]<<"-"<<nama[9]<<endl;
+	
+}
